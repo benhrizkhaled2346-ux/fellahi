6a129757d708741fbca73b8b9f3c268c00ec6261
mohamedbenhariz05 <mohamedbenhariz05@gmail.com>
2026-04-20 23:08:27 +0100

    change username to email

--- src/main/java/com/appfor/ne3ma/model/User.java
+++ src/main/java/com/appfor/ne3ma/model/User.java
@@ -22,7 +22,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email", unique = true, nullable = false)
+    @Column(name = "username", unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
