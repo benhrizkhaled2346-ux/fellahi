ae8c3d7bf47fd24a7a74fdfaf61228bf5b69835c
mohamedbenhariz05 <mohamedbenhariz05@gmail.com>
2026-04-21 18:43:44 +0100

    change username to email

--- src/main/java/com/appfor/ne3ma/model/User.java
+++ src/main/java/com/appfor/ne3ma/model/User.java
@@ -33,7 +33,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false ,unique = true)
     private String phone;
 
 
