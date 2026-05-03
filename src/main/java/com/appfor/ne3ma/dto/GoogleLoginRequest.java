6f1ccd412572ebfb1b74eeff12504cfd75506530
benhrizkhaled2346-ux <benhrizkhaled2346@gmail.com>
2026-05-03 21:06:48 +0100

    add oAuth 2

---
+++ src/main/java/com/appfor/ne3ma/dto/GoogleLoginRequest.java
@@ -0,0 +1,16 @@
+package com.appfor.ne3ma.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleLoginRequest {
+    @NotBlank
+    private String idToken;
+
+    private boolean rememberMe;
+}
